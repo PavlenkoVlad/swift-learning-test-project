c05c02bee2a4afe761647410939c1ffbe9337f62
Vlad Pavlenko <pavlenko0902@gmail.com>
2019-06-24 13:44:03 +0300

    init commit

---
+++ Test Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test Project
+//
+//  Created by Владислав Павленко on 6/11/19.
+//  Copyright © 2019 Владислав Павленко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
